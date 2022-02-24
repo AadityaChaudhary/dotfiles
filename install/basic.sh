70600b1678c4c919fe84ca29da739c2a808c580a
Aaditya Chaudhary <bluebird1339@gmail.com>
2022-02-24 12:16:35 -0500

    added vimrc and zshrc and other basic installs

---
+++ install/basic.sh
@@ -0,0 +1,1 @@
+sudo pacman -S git vim vi curl
---
+++ install/zsh.sh
@@ -0,0 +1,8 @@
+sudo pacman -S wget zsh xclip
+chsh -s /bin/zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+wget "https://raw.githubusercontent.com/caiogondim/bullet-train-oh-my-zsh-theme/master/bullet-train.zsh-theme" -O $ZSH_CUSTOM/themes/bullet-train.zsh-theme 
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+rm ~/.zshrc
+ln -s ~/dotfiles/config/zshrc ~/.zshrc
